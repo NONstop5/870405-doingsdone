63d754ddbd34c3b213750dfb64f7e63574f7f8a0
NONstop5 <non_2002@mail.ru>
2018-09-19 00:16:10 +0300

    Шаблонизация проекта

---
+++ functions.php
@@ -0,0 +1,30 @@
+<?php
+// Функция подсчета количества задач в проекте
+function calcTasksQuantity ($tasks, $project) {
+    $tasksQuantity = 0;
+    foreach ($tasks as $task) {
+        if ($project === $task['project']) {
+            $tasksQuantity++;
+        }
+    }
+    return $tasksQuantity;
+}
+
+// Функция шаблонизатор
+function include_template($name, $data) {
+    $name = 'templates/' . $name;
+    $result = '';
+
+    if (!file_exists($name)) {
+        return $result;
+    }
+
+    ob_start();
+    extract($data);
+    require_once $name;
+
+    $result = ob_get_clean();
+
+return $result;
+}
+?>
