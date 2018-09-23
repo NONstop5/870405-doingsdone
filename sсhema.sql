9b6417ad4b8fca6bffd0c52ba4a157c28c8e2161
NONstop5 <non_2002@mail.ru>
2018-09-23 10:11:59 +0300

    module4-task1

---
+++ sсhema.sql
@@ -0,0 +1,73 @@
+-- --------------------------------------------------------
+-- Хост:                         127.0.0.1
+-- Версия сервера:               5.7.20 - MySQL Community Server (GPL)
+-- Операционная система:         Win64
+-- HeidiSQL Версия:              9.5.0.5196
+-- --------------------------------------------------------
+
+/*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
+/*!40101 SET NAMES utf8 */;
+/*!50503 SET NAMES utf8mb4 */;
+/*!40014 SET @OLD_FOREIGN_KEY_CHECKS=@@FOREIGN_KEY_CHECKS, FOREIGN_KEY_CHECKS=0 */;
+/*!40101 SET @OLD_SQL_MODE=@@SQL_MODE, SQL_MODE='NO_AUTO_VALUE_ON_ZERO' */;
+
+
+-- Дамп структуры базы данных doingsdone
+CREATE DATABASE IF NOT EXISTS `doingsdone` /*!40100 DEFAULT CHARACTER SET utf8 */;
+USE `doingsdone`;
+
+-- Дамп структуры для таблица doingsdone.projects
+CREATE TABLE IF NOT EXISTS `projects` (
+  `project_id` int(11) NOT NULL AUTO_INCREMENT,
+  `user_id` int(11) NOT NULL,
+  `project_name` varchar(50) NOT NULL,
+  PRIMARY KEY (`project_id`),
+  UNIQUE KEY `project_id` (`project_id`),
+  KEY `user_id` (`user_id`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8;
+
+-- Дамп данных таблицы doingsdone.projects: ~0 rows (приблизительно)
+/*!40000 ALTER TABLE `projects` DISABLE KEYS */;
+/*!40000 ALTER TABLE `projects` ENABLE KEYS */;
+
+-- Дамп структуры для таблица doingsdone.tasks
+CREATE TABLE IF NOT EXISTS `tasks` (
+  `task_id` int(11) NOT NULL AUTO_INCREMENT,
+  `project_id` int(11) NOT NULL,
+  `user_id` int(11) NOT NULL,
+  `task_name` int(11) NOT NULL,
+  `task_create_date` datetime NOT NULL,
+  `task_complete_date` datetime DEFAULT NULL,
+  `task_deadline` datetime DEFAULT NULL,
+  `task_file` varchar(100) DEFAULT NULL,
+  `task_complete_status` tinyint(1) NOT NULL DEFAULT '0',
+  PRIMARY KEY (`task_id`),
+  UNIQUE KEY `task_id` (`task_id`),
+  KEY `project_id` (`project_id`),
+  KEY `user_id` (`user_id`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8;
+
+-- Дамп данных таблицы doingsdone.tasks: ~0 rows (приблизительно)
+/*!40000 ALTER TABLE `tasks` DISABLE KEYS */;
+/*!40000 ALTER TABLE `tasks` ENABLE KEYS */;
+
+-- Дамп структуры для таблица doingsdone.users
+CREATE TABLE IF NOT EXISTS `users` (
+  `user_id` int(11) NOT NULL AUTO_INCREMENT,
+  `register_date` datetime NOT NULL,
+  `user_email` varchar(50) NOT NULL,
+  `user_name` varchar(50) NOT NULL,
+  `user_password` varchar(50) NOT NULL,
+  `user_contacts` varchar(100) DEFAULT NULL,
+  PRIMARY KEY (`user_id`),
+  UNIQUE KEY `user_id` (`user_id`),
+  UNIQUE KEY `user_email` (`user_email`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8;
+
+-- Дамп данных таблицы doingsdone.users: ~0 rows (приблизительно)
+/*!40000 ALTER TABLE `users` DISABLE KEYS */;
+/*!40000 ALTER TABLE `users` ENABLE KEYS */;
+
+/*!40101 SET SQL_MODE=IFNULL(@OLD_SQL_MODE, '') */;
+/*!40014 SET FOREIGN_KEY_CHECKS=IF(@OLD_FOREIGN_KEY_CHECKS IS NULL, 1, @OLD_FOREIGN_KEY_CHECKS) */;
+/*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
